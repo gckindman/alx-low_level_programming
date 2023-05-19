3804ec24168183bb15e188a789f19d9d73a8ff2f
Williams Kindman <wkindman4@gmail.com>
2023-05-19 15:42:28 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a sentence using printf followed by a new line.
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
