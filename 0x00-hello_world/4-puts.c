ffa88bf7036006f9004978d8d7e2b50a243ffd20
Williams Kindman <wkindman4@gmail.com>
2023-05-19 15:32:58 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main- prints a sentence using put followed by a new line
+ *
+ * Return: always return 0
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
